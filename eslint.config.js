2229a0cb8fc362ea136fe9030fb996a9a26116c4
Caio Vicentino <caiovicentino@users.noreply.github.com>
2026-08-30 11:24:50 -0300

    chore(lint): ignore capacitor ios native project + synced assets

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["**/node_modules/**", "**/dist/**", "tools/**"] },
+  { ignores: ["**/node_modules/**", "**/dist/**", "tools/**", "apps/web/ios/**"] },
   ...tseslint.configs.recommended,
   {
     rules: {
